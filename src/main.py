037d5a2d086a3d11eb73ad7e5609beb4d4163636
lfariazzz <levi.farias@aluno.ufca.edu.br>
2026-04-26 14:35:03 -0300

    feat: implementa funções de feedback visual e sonoro - leds e buzzers

--- src/main.py
+++ src/main.py
@@ -40,3 +40,48 @@ tempo_inicio   = 0
 ultimo_botao   = 0
 
 print("Sistema pronto. Pressione F1 para iniciar autenticacao.")
+
+#Leds e Buzzers
+def todos_leds_off():
+    led_green.off()
+    led_red.off()
+    led_yellow.off()
+
+def beep(duracao_ms=100, freq=1000):
+    buzzer.freq(freq)
+    buzzer.duty(512)
+    from utime import sleep_ms
+    sleep_ms(duracao_ms)
+    buzzer.duty(0)
+
+def feedback_aprovado():
+    todos_leds_off()
+    led_green.on()
+    beep(200, 1200)
+    print("ACESSO AUTORIZADO")
+
+def feedback_negado():
+    todos_leds_off()
+    led_red.on()
+    beep(500, 400)
+    print("ACESSO NEGADO")
+
+def feedback_aguardando():
+    todos_leds_off()
+    led_yellow.on()
+    beep(50, 800)
+    print("Aguardando fator 2...")
+
+def feedback_bloqueado():
+    todos_leds_off()
+    led_red.on()
+    buzzer.freq(300)
+    buzzer.duty(512)
+    print("SISTEMA BLOQUEADO!")
+
+def feedback_desbloqueado():
+    buzzer.duty(0)
+    todos_leds_off()
+    print("Sistema desbloqueado. Aguardando...")
+
+print("Funcoes de feedback carregadas.")
